ca7966d27957c35e00a6b9529d3ab68f7cb4d67a
tintinkurtti <tintinkurtti@hotmail.com>
2025-02-12 14:46:57 +0100

    Fixed routing to pages

---
+++ src/Pages/BookingPage.tsx
@@ -0,0 +1,12 @@
+
+import React from "react";
+
+const BookingPage: React.FC = () => {
+    return (
+        <>
+            <p>Bokning sker genom våran telefon 070 020 69 69</p>
+        </>
+    );
+};
+
+export default BookingPage;
---
+++ src/Pages/MenuPage.tsx
@@ -0,0 +1,12 @@
+
+import React from "react";
+
+const MenuPage: React.FC = () => {
+    return (
+        <>
+            <p>Maten är god</p>
+        </>
+    );
+};
+
+export default MenuPage;
