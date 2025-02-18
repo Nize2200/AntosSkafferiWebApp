83088012dca628c3b9c51bb3b8b4308e15d02180
tintinkurtti <tintinkurtti@hotmail.com>
2025-02-18 14:11:39 +0100

    Added api call

--- src/api/ApiFetch.tsx
+++ src/api/ApiFetch.tsx
@@ -1,7 +1,7 @@
 
 export const fetchLunches = async () => {
     try {
-        const response = await fetch("http://localhost:3001/lunches");
+        const response = await fetch("http://localhost:8080/api/lunches/");
         return await response.json();
     } catch (error) {
         console.error('Error fetching lunches:', error);
